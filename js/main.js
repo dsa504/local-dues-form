d399c88627b798f32e746cecb15eff877f6680f1
Steve Price <methion@gmail.com>
2018-11-22 23:54:04 -0500

    tried to make some shit happen but its Thanksgiving and I'm drunk...

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
-import Vue from "../node_modules/vue/dist/vue.esm.browser.js"
+import Vue from "../node_modules/vue/dist/vue.esm.browser.js";
+import "../node_modules/vue-material/dist/components.js";
+
+const duesTypes = [
+    "Chapter Dues",
+    "DSA NOLA Fund",
+    "Labor Standing",
+    "Municipal Action",
+    "Communications",
+    "Technical",
+    "Political Education",
+    "Direct Service",
+    "Healthcare For All",
+] ;
+
 
 const duesVm = new Vue({
+    el: "#app",
+    data: {
+        presets: {
+            duesTypes,
+        },
+        info: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            billingAddress: "",
+            city: "",
+            state: "",
+            zip: "",
+            ccNumber: "",
+            ccMonth: "",
+            ccYear: "",
+            ccSecurityCode: "",
 
+        },
+        contributions: [
+            {
+                name: "chapter",
+                amount: 0,
+            }
+        ]
+    },
 });
 
 window.duesVm = duesVm;
